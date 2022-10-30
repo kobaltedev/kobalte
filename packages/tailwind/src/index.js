a63f0037e6211f02bfa564fa3e1f9deec1344f30
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2022-10-30 12:26:09 +0100

    feat: update tailwind plugin

--- packages/tailwind/src/index.js
+++ packages/tailwind/src/index.js
@@ -1,43 +1,79 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const plugin = require("tailwindcss/plugin");
-const defaultColors = require("tailwindcss/colors");
 
-const fourLevelShades = ["50", "100", "200", "300"];
-const fiveLevelShades = [...fourLevelShades, "400"];
-const tenLevelShades = [...fiveLevelShades, "500", "600", "700", "800", "900"];
-const backgroundShades = [...fiveLevelShades, "white"];
+const TEN_SHADES_COLORS = [
+  /* TailwindCSS colors */
+  "slate",
+  "cloud",
+  "zinc",
+  "gray",
+  "stone",
+  "red",
+  "orange",
+  "amber",
+  "yellow",
+  "lime",
+  "green",
+  "emerald",
+  "teal",
+  "cyan",
+  "sky",
+  "blue",
+  "indigo",
+  "violet",
+  "purple",
+  "fuchsia",
+  "pink",
+  "rose",
 
-function computeColorPalette(colorName, shades, withAlpha = true) {
-  return shades.reduce((acc, key) => {
-    acc[key] = `rgb(var(--kobalte-colors-${colorName}-${key})${
-      withAlpha ? " / <alpha-value>" : ""
-    })`;
+  /* Semantic colors */
+  "primary",
+  "neutral",
+  "success",
+  "info",
+  "warning",
+  "danger",
+];
+
+const FOUR_SHADES = ["50", "100", "200", "300"];
+const FIVE_SHADES = [...FOUR_SHADES, "400"];
+const TEN_SHADES = [...FIVE_SHADES, "500", "600", "700", "800", "900"];
+const BG_SHADES = [...FIVE_SHADES, "white"];
+
+function createColorObject(shades, withAlpha) {
+  return { shades, withAlpha };
+}
+
+function getColors() {
+  const colorDefinitions = {
+    ...TEN_SHADES_COLORS.reduce((acc, name) => {
+      acc[name] = createColorObject(TEN_SHADES, true);
+      return acc;
+    }, {}),
+    border: createColorObject(FOUR_SHADES, false),
+    fill: createColorObject(FOUR_SHADES, false),
+    text: createColorObject(FOUR_SHADES, false),
+    background: createColorObject(BG_SHADES, false),
+  };
+
+  return Object.keys(colorDefinitions).reduce((acc, name) => {
+    const color = colorDefinitions[name];
+
+    acc[name] = color.shades.reduce((acc, shade) => {
+      acc[shade] = `rgb(var(--kobalte-colors-${name}-${shade})${
+        color.withAlpha ? " / <alpha-value>" : ""
+      })`;
+      return acc;
+    }, {});
+
     return acc;
   }, {});
 }
 
-module.exports = plugin(
-  function () {
-    return;
+module.exports = plugin(function () {}, {
+  darkMode: ["class", ".kobalte-theme-dark"],
+  theme: {
+    colors: getColors(),
   },
-  {
-    darkMode: ["class", ".kobalte-theme-dark"],
-    theme: {
-      colors: {
-        ...defaultColors,
-        primary: computeColorPalette("primary", tenLevelShades),
-        neutral: computeColorPalette("neutral", tenLevelShades),
-        success: computeColorPalette("success", tenLevelShades),
-        info: computeColorPalette("info", tenLevelShades),
-        warning: computeColorPalette("warning", tenLevelShades),
-        danger: computeColorPalette("danger", tenLevelShades),
-
-        border: computeColorPalette("border", fourLevelShades),
-        fill: computeColorPalette("fill", fourLevelShades, false),
-        text: computeColorPalette("text", fourLevelShades, false),
-        background: computeColorPalette("background", backgroundShades),
-      },
-    },
-  }
-);
+});
