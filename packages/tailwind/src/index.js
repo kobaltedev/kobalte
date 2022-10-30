99d8d54cbd187d47f7c3f4ad84a4b45b947a73f6
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2022-10-30 10:18:43 +0100

    chore: update colors

--- packages/tailwind/src/index.js
+++ packages/tailwind/src/index.js
@@ -1,23 +1,18 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const plugin = require("tailwindcss/plugin");
+const defaultColors = require("tailwindcss/colors");
 
-const shades = [
-  "50",
-  "100",
-  "200",
-  "300",
-  "400",
-  "500",
-  "600",
-  "700",
-  "800",
-  "900",
-];
+const fourLevelShades = ["50", "100", "200", "300"];
+const fiveLevelShades = [...fourLevelShades, "400"];
+const tenLevelShades = [...fiveLevelShades, "500", "600", "700", "800", "900"];
+const backgroundShades = [...fiveLevelShades, "white"];
 
-function computeColorPalette(colorName) {
+function computeColorPalette(colorName, shades, withAlpha = true) {
   return shades.reduce((acc, key) => {
-    acc[key] = `rgb(var(--kobalte-colors-${colorName}-${key}) / <alpha-value>)`;
+    acc[key] = `rgb(var(--kobalte-colors-${colorName}-${key})${
+      withAlpha ? " / <alpha-value>" : ""
+    })`;
     return acc;
   }, {});
 }
@@ -29,15 +24,19 @@ module.exports = plugin(
   {
     darkMode: ["class", ".kobalte-theme-dark"],
     theme: {
-      extend: {
-        colors: {
-          primary: computeColorPalette("primary"),
-          secondary: computeColorPalette("secondary"),
-          success: computeColorPalette("success"),
-          info: computeColorPalette("info"),
-          warning: computeColorPalette("warning"),
-          danger: computeColorPalette("danger"),
-        },
+      colors: {
+        ...defaultColors,
+        primary: computeColorPalette("primary", tenLevelShades),
+        neutral: computeColorPalette("neutral", tenLevelShades),
+        success: computeColorPalette("success", tenLevelShades),
+        info: computeColorPalette("info", tenLevelShades),
+        warning: computeColorPalette("warning", tenLevelShades),
+        danger: computeColorPalette("danger", tenLevelShades),
+
+        border: computeColorPalette("border", fourLevelShades),
+        fill: computeColorPalette("fill", fourLevelShades, false),
+        text: computeColorPalette("text", fourLevelShades, false),
+        background: computeColorPalette("background", backgroundShades),
       },
     },
   }
