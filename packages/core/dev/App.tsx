c596bfb99690333c8691e466f9f7b8813fb759c3
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2022-11-07 20:40:19 +0100

    chore: rollback to css + start impl carbon button

--- packages/core/dev/App.tsx
+++ packages/core/dev/App.tsx
@@ -1,6 +1,6 @@
 import { For } from "solid-js";
 
-import { Button, useColorMode } from "../src";
+import { Button, IconButton, useColorMode } from "../src";
 import { SpinnerIcon } from "../src/icon/icon-set";
 
 export default function App() {
@@ -18,6 +18,21 @@ export default function App() {
           gap: "16px",
         }}
       >
+        <Button size="md" leftIcon={<SpinnerIcon />} isFullWidth>
+          Button
+        </Button>
+        <Button size="md" rightIcon={<SpinnerIcon />} isFullWidth>
+          Button
+        </Button>
+        <Button size="md" isLoading>
+          Button
+        </Button>
+        <Button size="md" isLoading loadingText="Loading..." loaderPlacement="start">
+          Button
+        </Button>
+        <Button size="md" isLoading loadingText="Loading..." loaderPlacement="end">
+          Button
+        </Button>
         <div
           style={{
             margin: "16px",
@@ -93,6 +108,101 @@ export default function App() {
                       <Button variant={variant as any} color={color as any} size="lg" isDisabled>
                         Button
                       </Button>
+                    </div>
+                  )}
+                </For>
+              </div>
+            )}
+          </For>
+        </div>
+      </div>
+      <div
+        style={{
+          margin: "16px",
+          display: "flex",
+          "flex-direction": "column",
+          "align-items": "center",
+          gap: "16px",
+        }}
+      >
+        <IconButton size="md" aria-label="spinner">
+          <SpinnerIcon />
+        </IconButton>
+        <div
+          style={{
+            margin: "16px",
+            display: "flex",
+            "align-items": "center",
+            gap: "16px",
+          }}
+        >
+          <IconButton size="xs" aria-label="spinner">
+            <SpinnerIcon />
+          </IconButton>
+          <IconButton size="sm" aria-label="spinner">
+            <SpinnerIcon />
+          </IconButton>
+          <IconButton size="md" aria-label="spinner">
+            <SpinnerIcon />
+          </IconButton>
+          <IconButton size="lg" aria-label="spinner" isDisabled>
+            <SpinnerIcon />
+          </IconButton>
+        </div>
+        <div>
+          <For each={["solid", "soft", "outlined", "plain"]}>
+            {variant => (
+              <div
+                style={{
+                  margin: "16px",
+                  display: "flex",
+                  "align-items": "center",
+                  gap: "16px",
+                }}
+              >
+                <For each={["primary", "secondary", "danger"]}>
+                  {color => (
+                    <div
+                      style={{
+                        margin: "16px",
+                        display: "flex",
+                        "align-items": "center",
+                        gap: "16px",
+                      }}
+                    >
+                      <IconButton
+                        variant={variant as any}
+                        color={color as any}
+                        size="xs"
+                        aria-label="spinner"
+                      >
+                        <SpinnerIcon />
+                      </IconButton>
+                      <IconButton
+                        variant={variant as any}
+                        color={color as any}
+                        size="sm"
+                        aria-label="spinner"
+                      >
+                        <SpinnerIcon />
+                      </IconButton>
+                      <IconButton
+                        variant={variant as any}
+                        color={color as any}
+                        size="md"
+                        aria-label="spinner"
+                      >
+                        <SpinnerIcon />
+                      </IconButton>
+                      <IconButton
+                        variant={variant as any}
+                        color={color as any}
+                        size="lg"
+                        aria-label="spinner"
+                        isDisabled
+                      >
+                        <SpinnerIcon />
+                      </IconButton>
                     </div>
                   )}
                 </For>
