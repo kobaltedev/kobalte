8fb27dc6716077feff82379522fd0736794f799c
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2022-11-17 17:23:06 +0100

    fix(elements): remove pressable

--- packages/elements/src/button/button.tsx
+++ packages/elements/src/button/button.tsx
@@ -19,12 +19,12 @@ import {
   mergeRefs,
 } from "@kobalte/utils";
 import { createMemo, JSX, splitProps } from "solid-js";
+import { Dynamic } from "solid-js/web";
 
-import { Pressable, PressableProps } from "../pressable";
-import { createFocusRing, createTagName } from "../primitives";
+import { createFocusRing, createPress, CreatePressProps, createTagName } from "../primitives";
 import { isButton } from "./is-button";
 
-export interface ButtonProps extends PressableProps {
+export interface ButtonProps extends CreatePressProps {
   /** Whether the button is disabled. */
   disabled?: boolean;
 }
@@ -45,14 +45,37 @@ export const Button = createPolymorphicComponent<"button", ButtonProps>(props =>
     props
   );
 
-  const [local, others] = splitProps(props, [
-    "as",
-    "ref",
-    "type",
-    "disabled",
-    "onFocusIn",
-    "onFocusOut",
-  ]);
+  const [local, createPressProps, others] = splitProps(
+    props,
+    [
+      "as",
+      "ref",
+      "type",
+      "disabled",
+      "onKeyDown",
+      "onKeyUp",
+      "onClick",
+      "onPointerDown",
+      "onPointerUp",
+      "onMouseDown",
+      "onDragStart",
+      "onFocusIn",
+      "onFocusOut",
+    ],
+    [
+      "onPressStart",
+      "onPressEnd",
+      "onPressUp",
+      "onPressChange",
+      "onPress",
+      "disabled",
+      "preventFocusOnPress",
+      "cancelOnPointerExit",
+      "allowTextSelectionOnPress",
+    ]
+  );
+
+  const { isPressed, pressHandlers } = createPress<HTMLButtonElement>(createPressProps);
 
   const { isFocused, isFocusVisible, focusHandlers } = createFocusRing({
     within: true,
@@ -81,6 +104,38 @@ export const Button = createPolymorphicComponent<"button", ButtonProps>(props =>
     return tagName() === "input";
   });
 
+  const onKeyDown: JSX.EventHandlerUnion<HTMLButtonElement, KeyboardEvent> = e => {
+    chainHandlers(e, [local.onKeyDown, pressHandlers.onKeyDown]);
+  };
+
+  const onKeyUp: JSX.EventHandlerUnion<HTMLButtonElement, KeyboardEvent> = e => {
+    chainHandlers(e, [local.onKeyUp, pressHandlers.onKeyUp]);
+  };
+
+  const onClick: JSX.EventHandlerUnion<HTMLButtonElement, MouseEvent> = e => {
+    if (local.onClick) {
+      console.warn("[kobalte]: onClick is deprecated, please use onPress");
+    }
+
+    chainHandlers(e, [local.onClick, pressHandlers.onClick]);
+  };
+
+  const onPointerDown: JSX.EventHandlerUnion<HTMLButtonElement, PointerEvent> = e => {
+    chainHandlers(e, [local.onPointerDown, pressHandlers.onPointerDown]);
+  };
+
+  const onPointerUp: JSX.EventHandlerUnion<HTMLButtonElement, PointerEvent> = e => {
+    chainHandlers(e, [local.onPointerUp, pressHandlers.onPointerUp]);
+  };
+
+  const onMouseDown: JSX.EventHandlerUnion<HTMLButtonElement, MouseEvent> = e => {
+    chainHandlers(e, [local.onMouseDown, pressHandlers.onMouseDown]);
+  };
+
+  const onDragStart: JSX.EventHandlerUnion<HTMLButtonElement, DragEvent> = e => {
+    chainHandlers(e, [local.onDragStart, pressHandlers.onDragStart]);
+  };
+
   const onFocusIn: JSX.EventHandlerUnion<HTMLButtonElement, FocusEvent> = e => {
     chainHandlers(e, [local.onFocusIn, focusHandlers.onFocusIn]);
   };
@@ -90,17 +145,25 @@ export const Button = createPolymorphicComponent<"button", ButtonProps>(props =>
   };
 
   return (
-    <Pressable
-      as={local.as}
+    <Dynamic
+      component={local.as}
       ref={mergeRefs(el => (ref = el), local.ref)}
       type={isNativeButton() || isInput() ? local.type : undefined}
       role={!isNativeButton() && !isLink() ? "button" : undefined}
       tabIndex={!isNativeButton() && !isLink() && !local.disabled ? 0 : undefined}
       disabled={isNativeButton() || isInput() ? local.disabled : undefined}
       aria-disabled={!isNativeButton() && !isInput() && local.disabled ? true : undefined}
+      data-pressed={isPressed() ? "" : undefined}
       data-disabled={local.disabled ? "" : undefined}
       data-focus={isFocused() ? "" : undefined}
       data-focus-visible={isFocusVisible() ? "" : undefined}
+      onKeyDown={onKeyDown}
+      onKeyUp={onKeyUp}
+      onClick={onClick}
+      onPointerDown={onPointerDown}
+      onPointerUp={onPointerUp}
+      onMouseDown={onMouseDown}
+      onDragStart={onDragStart}
       onFocusIn={onFocusIn}
       onFocusOut={onFocusOut}
       {...others}
