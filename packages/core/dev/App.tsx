407f56d6bd96693c2414b182605c7d21f4750e83
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2023-01-19 15:12:03 +0100

    fix(button,link): recursive createPress logic using the 'as' prop

--- packages/core/dev/App.tsx
+++ packages/core/dev/App.tsx
@@ -1,47 +1,5 @@
-import { ComponentProps, For } from "solid-js";
-
-import { Calendar, I18nProvider } from "../src";
-
-function CalendarMonth(props: ComponentProps<typeof Calendar.Month>) {
-  return (
-    <Calendar.Month {...props}>
-      <Calendar.Header class="header">
-        <Calendar.PrevPageButton class="button">&lsaquo;</Calendar.PrevPageButton>
-        <Calendar.Title />
-        <Calendar.NextPageButton class="button">&rsaquo;</Calendar.NextPageButton>
-      </Calendar.Header>
-      <Calendar.Grid cell-padding="0" class="grid">
-        <Calendar.GridHeader>
-          <Calendar.WeekDays class="week-days">
-            {day => <Calendar.WeekDay>{day()}</Calendar.WeekDay>}
-          </Calendar.WeekDays>
-        </Calendar.GridHeader>
-        <Calendar.GridBody>
-          {weekIndex => (
-            <Calendar.Row weekIndex={weekIndex()}>
-              {date => (
-                <Calendar.Cell date={date()}>
-                  <Calendar.Day class="cell" />
-                </Calendar.Cell>
-              )}
-            </Calendar.Row>
-          )}
-        </Calendar.GridBody>
-      </Calendar.Grid>
-    </Calendar.Month>
-  );
-}
+import { I18nProvider } from "../src";
 
 export default function App() {
-  const months = [...new Array(2).keys()];
-
-  return (
-    <div class="app">
-      <I18nProvider>
-        <Calendar.Root class="calendar" visibleMonths={months.length}>
-          <For each={months}>{offset => <CalendarMonth offset={offset} />}</For>
-        </Calendar.Root>
-      </I18nProvider>
-    </div>
-  );
+  return <I18nProvider></I18nProvider>;
 }
