c3fd5404e46d8f8dc2853c3f2a46eb062bb74fee
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2023-01-20 18:52:21 +0100

    chore: cleanup

--- packages/core/dev/App.tsx
+++ packages/core/dev/App.tsx
@@ -1,32 +1,5 @@
-import { createSignal, onMount } from "solid-js";
-
-import { I18nProvider, Progress } from "../src";
+import { I18nProvider } from "../src";
 
 export default function App() {
-  const [value, setValue] = createSignal(0);
-
-  onMount(() => {
-    const id = window.setInterval(() => {
-      if (value() === 100) {
-        window.clearInterval(id);
-        return;
-      }
-
-      setValue(prev => prev + 1);
-    }, 250);
-  });
-
-  return (
-    <I18nProvider>
-      <Progress.Root value={value()} class="progress">
-        <div class="progress__head">
-          <Progress.Label class="progress__label">Loading...</Progress.Label>
-          <Progress.ValueLabel class="progress__value-label" />
-        </div>
-        <Progress.Track class="progress__track">
-          <Progress.Fill class="progress__fill" />
-        </Progress.Track>
-      </Progress.Root>
-    </I18nProvider>
-  );
+  return <I18nProvider></I18nProvider>;
 }
