995a3b2b0087a3373afc2edb9daaacc085c4c672
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2023-03-23 19:18:22 +0100

    chore: cleanup

--- packages/core/dev/App.tsx
+++ packages/core/dev/App.tsx
@@ -1,29 +1,5 @@
-import { I18nProvider, Select } from "../src";
+import { I18nProvider } from "../src";
 
 export default function App() {
-  return (
-    <I18nProvider>
-      <input value="I am input" />
-      <Select.Root
-        options={["Apple", "Banana", "Blueberry", "Grapes", "Pineapple"]}
-        placeholder="Select a fruit…"
-        valueComponent={props => props.item.rawValue}
-        itemComponent={props => (
-          <Select.Item item={props.item} class="select__item">
-            <Select.ItemLabel>{props.item.rawValue}</Select.ItemLabel>
-          </Select.Item>
-        )}
-      >
-        <Select.Trigger class="select__trigger" aria-label="Fruit">
-          <Select.Value class="select__value" />
-        </Select.Trigger>
-        <Select.Portal>
-          <Select.Content class="select__content">
-            <Select.Listbox class="select__listbox" />
-          </Select.Content>
-        </Select.Portal>
-      </Select.Root>
-      <input value="I am input" />
-    </I18nProvider>
-  );
+  return <I18nProvider></I18nProvider>;
 }
