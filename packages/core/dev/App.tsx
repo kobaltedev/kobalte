5fe40089f7d9aab5843f123e4c3d05c30c2cd561
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2023-03-24 10:27:21 +0100

    chore: cleanup

--- packages/core/dev/App.tsx
+++ packages/core/dev/App.tsx
@@ -1,52 +1,5 @@
-import { createSignal } from "solid-js";
-
-import { I18nProvider, MultiSelect as KSelect } from "../src";
-
-const Select = <T, U>(props: KSelect.MultiSelectRootProps<T, U>) => {
-  return (
-    <div>
-      <KSelect.Root
-        {...props}
-        valueComponent={props => props.items.map(item => item.rawValue).join(", ")}
-        itemComponent={icProps => (
-          <KSelect.Item item={icProps.item}>
-            <KSelect.ItemIndicator forceMount>
-              <input type="checkbox" checked={new Set(props.value).has(icProps.item.key)} />
-            </KSelect.ItemIndicator>
-            <KSelect.ItemLabel>{icProps.item.rawValue as any}</KSelect.ItemLabel>
-          </KSelect.Item>
-        )}
-      >
-        <KSelect.Trigger>
-          <KSelect.Value />
-          <div>click here</div>
-        </KSelect.Trigger>
-        <KSelect.Portal>
-          <KSelect.Content>
-            {props.children}
-            <KSelect.Listbox />
-          </KSelect.Content>
-        </KSelect.Portal>
-      </KSelect.Root>
-    </div>
-  );
-};
+import { I18nProvider } from "../src";
 
 export default function App() {
-  const [selected, setSelected] = createSignal(new Set(["bananas"]));
-  const options = ["apples", "bananas", "cucumbers", "date", "elderberry"];
-
-  return (
-    <I18nProvider>
-      <Select
-        options={options}
-        optionDisabled={option => {
-          console.log(option === options[0]);
-          return option === options[0];
-        }}
-        value={selected()}
-        onValueChange={value => setSelected(new Set([...value]))}
-      />
-    </I18nProvider>
-  );
+  return <I18nProvider></I18nProvider>;
 }
