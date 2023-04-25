83aa22cd8d5031b8a86443456a3b47a53d8b9198
Sacha Weatherstone <sachaw100@hotmail.com>
2023-04-25 22:38:22 +1000

    fix: add required rollup-plugin-ts

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,6 +1,8 @@
 import withSolid from "rollup-preset-solid";
+import ts from "rollup-plugin-ts";
 
 export default withSolid({
-  input: "src/index.tsx",
-  targets: ["esm", "cjs"],
+	input: "src/index.tsx",
+	targets: ["esm", "cjs"],
+	plugins: [ts()],
 });
