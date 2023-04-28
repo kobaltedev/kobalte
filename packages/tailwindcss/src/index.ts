de2ba76ca43d9f5c1b8f09ccf635ec1d55cc164d
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2023-04-28 12:17:11 +0200

    feat(tailwind): add Kobalte UI color palettes

--- packages/tailwindcss/src/index.ts
+++ packages/tailwindcss/src/index.ts
@@ -8,6 +8,8 @@
 
 import plugin from "tailwindcss/plugin";
 
+import { colors } from "./colors";
+
 const STATES = [
   "valid",
   "invalid",
@@ -31,52 +33,70 @@ const SWIPE_DIRECTIONS = ["up", "down", "left", "right"];
 export interface KobalteTailwindPluginOptions {
   /** The prefix of generated classes. */
   prefix?: string;
+
+  /** Whether to include Kobalte UI color palettes in the Tailwind theme. */
+  colors?: boolean;
 }
 
-export default plugin.withOptions<KobalteTailwindPluginOptions>(({ prefix = "ui" } = {}) => {
-  return ({ addVariant }) => {
-    for (const state of STATES) {
-      addVariant(`${prefix}-${state}`, [`&[data-${state}]`]);
-      addVariant(`${prefix}-not-${state}`, [`&:not([data-${state}])`]);
-      addVariant(`${prefix}-group-${state}`, `:merge(.group)[data-${state}] &`);
-      addVariant(`${prefix}-peer-${state}`, `:merge(.peer)[data-${state}] ~ &`);
+export default plugin.withOptions<KobalteTailwindPluginOptions>(
+  ({ prefix = "ui", colors = false } = {}) => {
+    return ({ addVariant }) => {
+      for (const state of STATES) {
+        addVariant(`${prefix}-${state}`, [`&[data-${state}]`]);
+        addVariant(`${prefix}-not-${state}`, [`&:not([data-${state}])`]);
+        addVariant(`${prefix}-group-${state}`, `:merge(.group)[data-${state}] &`);
+        addVariant(`${prefix}-peer-${state}`, `:merge(.peer)[data-${state}] ~ &`);
+      }
+
+      for (const orientation of ORIENTATIONS) {
+        addVariant(`${prefix}-${orientation}`, [`&[data-orientation='${orientation}']`]);
+        addVariant(`${prefix}-not-${orientation}`, [`&:not([data-orientation='${orientation}'])`]);
+        addVariant(
+          `${prefix}-group-${orientation}`,
+          `:merge(.group)[data-orientation='${orientation}'] &`
+        );
+        addVariant(
+          `${prefix}-peer-${orientation}`,
+          `:merge(.peer)[data-orientation='${orientation}'] ~ &`
+        );
+      }
+
+      for (const state of SWIPE_STATES) {
+        addVariant(`${prefix}-swipe-${state}`, [`&[data-swipe='${state}']`]);
+        addVariant(`${prefix}-not-swipe-${state}`, [`&:not([data-swipe='${state}'])`]);
+        addVariant(`${prefix}-group-swipe-${state}`, `:merge(.group)[data-swipe='${state}'] &`);
+        addVariant(`${prefix}-peer-swipe-${state}`, `:merge(.peer)[data-swipe='${state}'] ~ &`);
+      }
+
+      for (const direction of SWIPE_DIRECTIONS) {
+        addVariant(`${prefix}-swipe-direction-${direction}`, [
+          `&[data-swipe-direction='${direction}']`,
+        ]);
+        addVariant(`${prefix}-not-swipe-direction-${direction}`, [
+          `&:not([data-swipe-direction='${direction}'])`,
+        ]);
+        addVariant(
+          `${prefix}-group-swipe-direction-${direction}`,
+          `:merge(.group)[data-swipe-direction='${direction}'] &`
+        );
+        addVariant(
+          `${prefix}-peer-swipe-direction-${direction}`,
+          `:merge(.peer)[data-swipe-direction='${direction}'] ~ &`
+        );
+      }
+    };
+  },
+  function (options) {
+    if (!options.colors) {
+      return {};
     }
 
-    for (const orientation of ORIENTATIONS) {
-      addVariant(`${prefix}-${orientation}`, [`&[data-orientation='${orientation}']`]);
-      addVariant(`${prefix}-not-${orientation}`, [`&:not([data-orientation='${orientation}'])`]);
-      addVariant(
-        `${prefix}-group-${orientation}`,
-        `:merge(.group)[data-orientation='${orientation}'] &`
-      );
-      addVariant(
-        `${prefix}-peer-${orientation}`,
-        `:merge(.peer)[data-orientation='${orientation}'] ~ &`
-      );
-    }
-
-    for (const state of SWIPE_STATES) {
-      addVariant(`${prefix}-swipe-${state}`, [`&[data-swipe='${state}']`]);
-      addVariant(`${prefix}-not-swipe-${state}`, [`&:not([data-swipe='${state}'])`]);
-      addVariant(`${prefix}-group-swipe-${state}`, `:merge(.group)[data-swipe='${state}'] &`);
-      addVariant(`${prefix}-peer-swipe-${state}`, `:merge(.peer)[data-swipe='${state}'] ~ &`);
-    }
-
-    for (const direction of SWIPE_DIRECTIONS) {
-      addVariant(`${prefix}-swipe-direction-${direction}`, [
-        `&[data-swipe-direction='${direction}']`,
-      ]);
-      addVariant(`${prefix}-not-swipe-direction-${direction}`, [
-        `&:not([data-swipe-direction='${direction}'])`,
-      ]);
-      addVariant(
-        `${prefix}-group-swipe-direction-${direction}`,
-        `:merge(.group)[data-swipe-direction='${direction}'] &`
-      );
-      addVariant(
-        `${prefix}-peer-swipe-direction-${direction}`,
-        `:merge(.peer)[data-swipe-direction='${direction}'] ~ &`
-      );
-    }
-  };
-});
+    return {
+      theme: {
+        colors: {
+          ...colors,
+        },
+      },
+    };
+  }
+);
