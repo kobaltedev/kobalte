0e3f5337e60b801200464d3f25a3449e91359369
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2023-05-07 14:03:18 +0200

    chore: cleanup

--- packages/core/dev/App.tsx
+++ packages/core/dev/App.tsx
@@ -1,46 +1,5 @@
-import { I18nProvider, Slider } from "../src";
+import { I18nProvider } from "../src";
 
 export default function App() {
-  let formRef: HTMLFormElement | undefined;
-
-  const onSubmit = (e: SubmitEvent) => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    const formData = new FormData(formRef);
-
-    alert(JSON.stringify(Object.fromEntries(formData), null, 2));
-  };
-
-  return (
-    <I18nProvider locale="en-US">
-      <form ref={formRef} onSubmit={onSubmit}>
-        <Slider.Root
-          style={{ display: "inline-flex", "flex-direction": "column" }}
-          defaultValue={[25, 75]}
-          name="volume"
-        >
-          <div style={{ display: "flex", "justify-content": "space-between" }}>
-            <Slider.Label>Volume</Slider.Label>
-            <Slider.ValueLabel />
-          </div>
-          <div class="SliderRoot">
-            <Slider.Track class="SliderTrack">
-              <Slider.Fill class="SliderFill" />
-            </Slider.Track>
-            <Slider.Thumb class="SliderThumb">
-              <Slider.Input />
-            </Slider.Thumb>
-            <Slider.Thumb class="SliderThumb">
-              <Slider.Input />
-            </Slider.Thumb>
-          </div>
-        </Slider.Root>
-        <div>
-          <button type="reset">Reset</button>
-          <button>Submit</button>
-        </div>
-      </form>
-    </I18nProvider>
-  );
+  return <I18nProvider locale="en-US"></I18nProvider>;
 }
