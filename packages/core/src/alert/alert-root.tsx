702a849bc719d8535384e7366136cafdc89eb5ec
jer3m01 <jer3m01@jer3m01.com>
2024-04-03 21:42:56 +0200

    refactor(alert): polymorphic

--- packages/core/src/alert/alert-root.tsx
+++ packages/core/src/alert/alert-root.tsx
@@ -1,14 +1,29 @@
-import { OverrideComponentProps } from "@kobalte/utils";
+import { ValidComponent } from "solid-js";
 
-import { AsChildProp, Polymorphic } from "../polymorphic";
+import { Polymorphic, PolymorphicProps } from "../polymorphic";
 
-export interface AlertRootProps
-	extends OverrideComponentProps<"div", AsChildProp> {}
+export interface AlertRootOptions {}
+
+export interface AlertRootCommonProps {}
+
+export interface AlertRootRenderProps extends AlertRootCommonProps {
+	role: "alert";
+}
+
+export type AlertRootProps = AlertRootOptions & Partial<AlertRootCommonProps>;
 
 /**
  * Alert displays a brief, important message
  * in a way that attracts the user's attention without interrupting the user's task.
  */
-export function AlertRoot(props: AlertRootProps) {
-	return <Polymorphic as="div" role="alert" {...props} />;
+export function AlertRoot<T extends ValidComponent = "div">(
+	props: PolymorphicProps<T, AlertRootProps>,
+) {
+	return (
+		<Polymorphic<AlertRootRenderProps>
+			as="div"
+			role="alert"
+			{...(props as object)}
+		/>
+	);
 }
