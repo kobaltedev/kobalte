f77a119e20de0756f47c2ab205e06d2a61c4db91
jer3m01 <jer3m01@jer3m01.com>
2024-04-06 20:41:36 +0200

    refactor(alert-dialog): polymorphic

--- packages/core/src/alert-dialog/alert-dialog-content.tsx
+++ packages/core/src/alert-dialog/alert-dialog-content.tsx
@@ -1,11 +1,43 @@
-import { DialogContentProps } from "../dialog";
-import { DialogContent } from "../dialog/dialog-content";
+import { Component, ValidComponent } from "solid-js";
+import {
+	DialogContent,
+	DialogContentCommonProps,
+	DialogContentOptions,
+	DialogContentRenderProps,
+} from "../dialog/dialog-content";
+import { PolymorphicProps } from "../polymorphic";
 
-export interface AlertDialogContentProps extends DialogContentProps {}
+export interface AlertDialogContentOptions extends DialogContentOptions {}
+
+export interface AlertDialogContentCommonProps
+	extends DialogContentCommonProps {}
+
+export interface AlertDialogContentRenderProps
+	extends AlertDialogContentCommonProps,
+		DialogContentRenderProps {
+	role: "alertdialog";
+}
+
+export type AlertDialogContentProps = AlertDialogContentOptions &
+	Partial<AlertDialogContentCommonProps>;
 
 /**
  * Overrides the regular `Dialog.Content` with role="alertdialog" to interrupt the user.
  */
-export function AlertDialogContent(props: AlertDialogContentProps) {
-	return <DialogContent role="alertdialog" {...props} />;
+export function AlertDialogContent<T extends ValidComponent = "div">(
+	props: PolymorphicProps<T, AlertDialogContentProps>,
+) {
+	return (
+		<DialogContent<
+			Component<
+				Omit<
+					AlertDialogContentRenderProps,
+					Exclude<keyof DialogContentRenderProps, "role">
+				>
+			>
+		>
+			role="alertdialog"
+			{...(props as AlertDialogContentProps)}
+		/>
+	);
 }
