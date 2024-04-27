7c25cd5528f55f7b48605a397ab8011d89638b01
jer3m01 <jer3m01@jer3m01.com>
2024-04-27 02:47:47 +0200

    refactor(dropdown-menu): polymorphic

--- packages/core/src/dropdown-menu/dropdown-menu-content.tsx
+++ packages/core/src/dropdown-menu/dropdown-menu-content.tsx
@@ -1,18 +1,35 @@
 import { OverrideComponentProps, focusWithoutScrolling } from "@kobalte/utils";
-import { splitProps } from "solid-js";
+import { Component, ValidComponent, splitProps } from "solid-js";
 
-import { MenuContent, MenuContentOptions } from "../menu";
+import {
+	MenuContent,
+	MenuContentCommonProps,
+	MenuContentOptions,
+	MenuContentRenderProps,
+} from "../menu";
 import { useMenuContext } from "../menu/menu-context";
 import { useMenuRootContext } from "../menu/menu-root-context";
+import { PolymorphicProps } from "../polymorphic";
 import { InteractOutsideEvent } from "../primitives";
 
-export interface DropdownMenuContentProps
-	extends OverrideComponentProps<"div", MenuContentOptions> {}
+export interface DropdownMenuContentOptions extends MenuContentOptions {}
+
+export interface DropdownMenuContentCommonProps
+	extends MenuContentCommonProps {}
+
+export interface DropdownMenuContentRenderProps
+	extends DropdownMenuContentCommonProps,
+		MenuContentRenderProps {}
+
+export type DropdownMenuContentProps = DropdownMenuContentOptions &
+	Partial<DropdownMenuContentCommonProps>;
 
 /**
  * Contains the content to be rendered when the dropdown menu is open.
  */
-export function DropdownMenuContent(props: DropdownMenuContentProps) {
+export function DropdownMenuContent<T extends ValidComponent = "div">(
+	props: PolymorphicProps<T, DropdownMenuContentProps>,
+) {
 	const rootContext = useMenuRootContext();
 	const context = useMenuContext();
 
@@ -45,7 +62,11 @@ export function DropdownMenuContent(props: DropdownMenuContentProps) {
 	};
 
 	return (
-		<MenuContent
+		<MenuContent<
+			Component<
+				Omit<DropdownMenuContentRenderProps, keyof MenuContentRenderProps>
+			>
+		>
 			onCloseAutoFocus={onCloseAutoFocus}
 			onInteractOutside={onInteractOutside}
 			{...others}
