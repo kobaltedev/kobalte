62fe6291375709681d17b7c43785b63518d8a40a
jer3m01 <jer3m01@jer3m01.com>
2024-07-31 00:39:10 +0200

    fix(number-field): don't trigger `onRawValueChange` on mount when NaN

--- packages/core/src/number-field/number-field-root.tsx
+++ packages/core/src/number-field/number-field-root.tsx
@@ -212,7 +212,7 @@ export function NumberFieldRoot<T extends ValidComponent = "div">(
 		},
 	});
 
-	local.onRawValueChange?.(parseRawValue(value()));
+	if (value()) local.onRawValueChange?.(parseRawValue(value()));
 
 	function isAllowedInput(char: string): boolean {
 		if (local.allowedInput !== undefined) return local.allowedInput.test(char);
