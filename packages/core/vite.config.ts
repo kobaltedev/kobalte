a6c82f87d498f8752afd8a2493ff597e45db713e
jer3m01 <jer3m01@jer3m01.com>
2024-10-07 23:21:37 +0200

    wip

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -3,11 +3,15 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	plugins: [solidPlugin()],
+	server: {
+		port: 4000,
+	},
 	test: {
 		environment: "jsdom",
 		globals: true,
 		setupFiles: ["../../node_modules/@testing-library/jest-dom/vitest"],
 		isolate: false,
+		watch: false,
 	},
 	resolve: {
 		conditions: ["development", "browser"],
