99ba32e66dbbeb5ecbfa14e1670da2ef6fe8c2ab
Swapnesh <swapneshsangle1@gmail.com>
2024-10-28 02:34:48 +0530

    feat: added chip component

--- apps/docs/src/routes/docs/core.tsx
+++ apps/docs/src/routes/docs/core.tsx
@@ -62,6 +62,10 @@ const CORE_NAV_SECTIONS: NavSection[] = [
 				href: "/docs/core/components/checkbox",
 			},
 			{
+				title: "Chip",
+				href: "/docs/core/components/chip",
+			},
+			{
 				title: "Collapsible",
 				href: "/docs/core/components/collapsible",
 			},
