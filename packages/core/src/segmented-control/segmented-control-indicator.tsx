c99e9f3b86f98fe4d62eb5d6c4d43224c4a2497d
zobweyt <zobweyt@gmail.com>
2024-11-19 14:40:32 +0300

    fix: correct reactive dependency

--- packages/core/src/segmented-control/segmented-control-indicator.tsx
+++ packages/core/src/segmented-control/segmented-control-indicator.tsx
@@ -64,7 +64,7 @@ export function SegmentedControlIndicator<T extends ValidComponent = "div">(
 	};
 
 	createEffect(
-		on(context.value, () => {
+		on(context.selectedItem, () => {
 			setResizing(!style());
 			computeStyle();
 			setResizing(false);
