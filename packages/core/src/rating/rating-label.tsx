743a42ec78b249b349d3070837db30b74f120fe0
jer3m01 <jer3m01@jer3m01.com>
2025-07-27 23:44:22 +0200

    style: format

--- packages/core/src/rating/rating-label.tsx
+++ packages/core/src/rating/rating-label.tsx
@@ -5,17 +5,13 @@ import type { ElementOf, PolymorphicProps } from "../polymorphic";
 
 export interface RatingLabelOptions {}
 
-export interface RatingLabelCommonProps<
-	T extends HTMLElement = HTMLElement,
-> {}
+export interface RatingLabelCommonProps<T extends HTMLElement = HTMLElement> {}
 
-export interface RatingLabelRenderProps
-	extends RatingLabelCommonProps {}
+export interface RatingLabelRenderProps extends RatingLabelCommonProps {}
 
 export type RatingLabelProps<
 	T extends ValidComponent | HTMLElement = HTMLElement,
-> = RatingLabelOptions &
-	Partial<RatingLabelCommonProps<ElementOf<T>>>;
+> = RatingLabelOptions & Partial<RatingLabelCommonProps<ElementOf<T>>>;
 
 export function RatingLabel<T extends ValidComponent = "span">(
 	props: PolymorphicProps<T, RatingLabelProps<T>>,
