99dd085ca9e35b8e425da2c14f77da522fe7e63f
cabboose <shayanftw@gmail.com>
2025-09-23 22:30:52 +0800

    chore(core): formatting

--- packages/core/src/menu/menu-item-base.tsx
+++ packages/core/src/menu/menu-item-base.tsx
@@ -147,12 +147,14 @@ export function MenuItemBase<T extends ValidComponent = "div">(
 		local.onSelect?.();
 
 		if (local.closeOnSelect) {
-			setTimeout(() => {
-				menuContext.close(true);
-			},
-			// Fix #446
-			// Requires a delay for NavigationMenu to work on mobile.
-			1);
+			setTimeout(
+				() => {
+					menuContext.close(true);
+				},
+				// Fix #446
+				// Requires a delay for NavigationMenu to work on mobile.
+				1,
+			);
 		}
 	};
 
