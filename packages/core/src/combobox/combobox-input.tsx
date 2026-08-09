7a3f7b8dbc88fe58803efb09c6710deef50dfb08
jer3m01 <jer3m01@jer3m01.com>
2026-08-10 01:57:09 +0200

    fix(combobox): clear input on blur

--- packages/core/src/combobox/combobox-input.tsx
+++ packages/core/src/combobox/combobox-input.tsx
@@ -256,6 +256,7 @@ export function ComboboxInput<T extends ValidComponent = "input">(
 		}
 
 		context.setIsInputFocused(false);
+		context.resetInputValue(context.listState().selectionManager().selectedKeys());
 	};
 
 	// If a touch happens on direct center of Combobox input, might be virtual click from iPad so open ComboBox menu
